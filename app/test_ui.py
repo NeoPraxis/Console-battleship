61ad1d0e514a7400455b97f122ea60ca0a18e537
Kaleb Currier <currierka@husson.edu>
2022-04-29 23:15:39 -0400

    update

--- app/test_ui.py
+++ app/test_ui.py
@@ -28,6 +28,7 @@ class TestUI(unittest.TestCase):
         self.assertTrue(callable(self.ui.on_navigation))
         self.assertTrue(callable(self.ui.on_space))
         self.assertTrue(callable(self.ui.on_enter))
+        self.assertTrue(callable(self.ui.get_shot))
     
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_get_name(self, mock_out):
@@ -106,20 +107,7 @@ class TestUI(unittest.TestCase):
         self.ui.on_space('Key.space')
         self.assertEqual(self.ui.orientation, 'v')
 
-    def test_enter_returns_coordinates_and_orientation(self):
+    def test_ui_can_display_targeting_grid(self):
         pass
 
-    def test_prompt_for_coordinates_returns_current_cursor_position(self):
-        pass
-
-    def test_get_grid_content_returns_correct_character_for_given_coordinate(self):
-        pass
-
-    def test_display_grid_renders_grid_with_grid_content(self):
-        pass
-
-    def test_console_clears_screen_to_refresh_ui_display_properly(self):
-        pass
         
-    def test_when_spacebar_pressed_orientation_of_ship_toggles(self):
-        pass
